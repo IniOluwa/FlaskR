399b2875542b3a9872f99259ea75da073db4d705
Ini-Oluwa C. Fageyinbo <inioluwafageyinbo@gmail.com>
2023-12-17 15:53:45 +0100

    Application Factory Cerated.

---
+++ flaskr/__init__.py
@@ -0,0 +1,33 @@
+# Import Dependencies
+import os
+from flask import Flask
+from os import environ
+
+# Create & Configure Flask App
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)
+    app.config.from_mapping(
+        SECRET_KEY = environ.get('SECRET_KEY'),
+        DATABASE = os.path.join(app.instance_path, 'flasr.sqlite')
+    )
+    
+    # Load Instance Config If It Exits, When Not Testing
+    if test_config is None:
+        app.config.from_pyfile('config.py', silent=True)
+    else:
+        # Load Test Config If Available
+        app.config.from_mapping(test_config)
+        
+    # Make Sure Instance Folder Exists
+    try:
+        os.makedirs(app.instance_path)
+    except OSError:
+        pass
+        
+    # Say Hello
+    @app.route('/hello')
+    def hello():
+        return "Hello, World!"
+        
+    # Return Created Application
+    return app
